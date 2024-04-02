0c942c0ba6ae99dc30a5d2fa1ed63c98b2dab7d2
pxassuncao <mirassuncaobjj07@gmail.com>
2024-04-02 17:43:17 -0300

    Adicionar arquivos de projeto.

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
